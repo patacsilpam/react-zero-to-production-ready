093ecbb6ba900976a1dc3b55e1c60566c9944789
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2025-01-08 20:41:15 +0800

    feat: added timeout for displaying default users in one second

--- src/components/users-list.tsx
+++ src/components/users-list.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Users } from "../types";
 import { UserCard } from "./user-card";
 const DEFAULT_USERS = [
@@ -6,9 +7,16 @@ const DEFAULT_USERS = [
 ];
 
 export function UsersList() {
+  const [users, setUsers] = useState<Users[]>([]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setUsers(DEFAULT_USERS);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <div className="flex flex-col gap-4 p-4">
-      {DEFAULT_USERS.map((user) => (
+      {users.map((user) => (
         <UserCard key={user.id} {...user} />
       ))}
     </div>
